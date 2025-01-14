5360ec2b9e1316bef9cd0d574895d7e449e1bd4c
John Austin Ngode <austinngode23@gmail.com>
2025-01-14 16:58:54 +0300

    Adds sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import './App.css';
 import Homepage from './components/Homepage';
 import Map from './components/Map';
 import Footer from './components/footer';
+import AboutUs from './components/About us/Aboutus';
+import Requirements from './components/Requirements/Requirements';
+import Extras from './components/Extras/Extras';
 
 
 
 function App() {
   return (
     <>
-      <Homepage/>
-      <Map/>
-      <Footer/>
+      <section><Homepage/></section>
+      <section><AboutUs/></section>
+      <section><Requirements/></section>
+      <section><Extras/></section>
+      <section><Map/></section>
+      <section><Footer/></section>
     </>
   );
 }
