d2c49b9b6faef86a9442363c3791b1626ccac3ae
lemmes33 <vinlemuel021@gmail.com>
2025-01-14 18:34:21 +0300

    extras

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from '../src/components/Navbar/Navbar'
 import About from '../src/components/About us/Aboutus'
 import Requirements from './components/Requirements/Requirements';
 import Extras from './components/Extras/Extras'
+import Location from './components/Location/Location';
 
 function App() {
   return (
     <>
     
-    
+    < Extras />
+
     
     </>
   );
