f77232126309777caf0218eab10a5cc099ad6a4c
lemmes33 <vinlemuel021@gmail.com>
2025-01-14 19:02:36 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import Map from './components/Map';
 import Footer from './components/footer';
 import AboutUs from './components/About us/Aboutus';
 import Requirements from './components/Requirements/Requirements';
-import Extras from './components/Extras/Extras'
+import Extras from './components/Extras/Extras';
+
+
 
 function App() {
   return (
     <>
-    
-    
-    
+      <section><Homepage/></section>
+      <section><AboutUs/></section>
+      <section><Requirements/></section>
+      <section><Extras/></section>
+      <section><Map/></section>
+      <section><Footer/></section>
     </>
   );
 }
