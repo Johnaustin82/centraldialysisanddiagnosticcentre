364ce6d43b7b2ebdccff436cd9aac921d6922ade
lemmes33 <vinlemuel021@gmail.com>
2025-01-14 13:39:26 +0300

    extras

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from '../src/components/Navbar/Navbar'
 import About from '../src/components/About us/Aboutus'
 import Requirements from './components/Requirements/Requirements';
+import Extras from './components/Extras/Extras'
 
 function App() {
   return (
     <>
-    <Requirements />
+    
+    
     
     </>
   );
--- src/components/Extras/Extras.jsx
+++ src/components/Extras/Extras.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./extras.css";
 
-function Extras() {
+const Extras = () => {
+  const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredCardIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredCardIndex(null);
+  };
+
   return (
-    <div>Extras</div>
-  )
-}
+    <div className="extras-container">
+      {/* First Card */}
+      <div 
+        className={`group ${hoveredCardIndex === 0 ? 'hovered' : ''}`} 
+        onMouseEnter={() => handleMouseEnter(0)} 
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="profile-picture">
+          <img
+            src="src/components/Img/extras_1.png"
+            alt="learn About Kidneys"
+          />
+        </div>
+        <div className="profile-details">
+          <span className="profile-name">learn About Kidneys</span>
+          <p className="profile-role">Learn more</p>
+          {/* Conditional rendering of extra info */}
+          {hoveredCardIndex === 0 && (
+            <div className="extra-info">
+              {/* Add your extra information here */}
+              <p>The kidneys play a vital role in filtering waste and maintaining the body's fluid. For dialysis patients, managing diet, fluid intake, and medications is crucial to support overall health and treatment outcomes. </p> 
+            </div>
+          )}
+        </div>
+      </div>
 
-export default Extras
+      {/* Second Card */}
+      <div 
+        className={`group ${hoveredCardIndex === 1 ? 'hovered' : ''}`} 
+        onMouseEnter={() => handleMouseEnter(1)} 
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="profile-picture">
+          <img
+            src="src/components/Img/10.png"
+            alt="10 plus"
+          />
+        </div>
+        <div className="profile-details">
+          <span className="profile-name">Decade plus working with you</span>
+          <p className="profile-role">Learn more</p>
+          {/* Conditional rendering of extra info */}
+          {hoveredCardIndex === 1 && (
+            <div className="extra-info">
+              {/* Add your extra information here */}
+              <p>TWe sincerely thank you for being part of our journey since 2012. Your trust and we are truly grateful for the opportunity to serve you. </p> 
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* Third Card */}
+      <div 
+        className={`group ${hoveredCardIndex === 2 ? 'hovered' : ''}`} 
+        onMouseEnter={() => handleMouseEnter(2)} 
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="profile-picture">
+          <img
+            src="src/components/Img/calender.png"
+            alt="calender"
+          />
+        </div>
+        <div className="profile-details">
+          <span className="profile-name">Mark your visiting days</span>
+          <p className="profile-role">Learn more</p>
+          {/* Conditional rendering of extra info */}
+          {hoveredCardIndex === 2 && (
+            <div className="extra-info">
+              {/* Add your extra information here */}
+              <p>Regular checkups and staying informed about your condition can make a significant difference in your journey. Always consult your healthcare provider for personalized advice.</p> 
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Extras;
