c52a450e91480ee9de7a3ed66692260d505f3f96
John Austin Ngode <austinngode23@gmail.com>
2025-01-15 05:26:33 +0300

    Feat:Embeds link to map image

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,11 +7,17 @@ function Map() {
         LOCATION
       </h1>
       <div className="w-full max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-lg">
-        <img 
-          src="src/components/Img/Map.png" 
-          alt="Map" 
-          className="w-full rounded-lg border border-gray-200"
-        />
+        <a
+          href="https://www.google.com/maps/place/Central+Dialysis+Centre/@-0.4181503,36.9351399,15z/data=!4m6!3m5!1s0x18285df2a89eba23:0xef34edabdd935a31!8m2!3d-0.4178092!4d36.9323811!16s%2Fg%2F11fpkhw5xl?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img 
+            src="src/components/Img/Map.png" 
+            alt="Map" 
+            className="w-full rounded-lg border border-gray-200"
+          />
+        </a>
       </div>
     </div>
   );
