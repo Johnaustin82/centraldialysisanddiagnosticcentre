26a5d92310e9e680bc79f7bbbfc160ff55554d70
John Austin Ngode <austinngode23@gmail.com>
2025-01-17 20:49:57 +0300

    Adds functionality to navbar links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,44 @@ import React from 'react';
 import './navbar.css';
 
 function Navbar() {
+  // Function to handle navigation
+  const handleNavigation = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <header className="glass-navbar">
       <div className="logo">
         <img src="src/components/Img/logo.png" alt="Your Logo" /> 
-        
       </div>
       <nav className="nav-links">
-        <button className="nav-button">Home</button>
-        <button className="nav-button">About  Us</button>
-        <button className="nav-button">Contact  Us</button>
-        <button className="nav-button">Location</button>
+        <button 
+          className="nav-button" 
+          onClick={() => handleNavigation('homepage-section')}
+        >
+          Home
+        </button>
+        <button 
+          className="nav-button" 
+          onClick={() => handleNavigation('aboutus-section')}
+        >
+          About Us
+        </button>
+        <button 
+          className="nav-button" 
+          onClick={() => handleNavigation('footer-section')}
+        >
+          Contact Us
+        </button>
+        <button 
+          className="nav-button" 
+          onClick={() => handleNavigation('map-section')}
+        >
+          Location
+        </button>
       </nav>
     </header>
   );
